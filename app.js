bfbfed86adaee6e72ea3600111f657b928c7a4d6
Autumn <autumncbullard@gmail.com>
2017-12-22 00:23:26 -0500

    Define a route that outputs the user's IP address, language, and OS

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.get('/api', function(req, res) {
+  var ip = req.connection.remoteAddress;
+  var language = req.headers['accept-language'].split(',')[0];
+  var os = req.headers['user-agent'].split(/[\(\)]/)[1];
+
+  res.json({IP: ip, language: language, OS: os});
+});
+
 app.listen(port, console.log('Server is listening at port ' + port + '.'));
